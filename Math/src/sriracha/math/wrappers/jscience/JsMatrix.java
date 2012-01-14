355edd68fba1a343237e578f5520afddde978dea
alennartz <alennartz@gmail.com>
2012-01-14 03:39:35 +0000

    Modified and recompiled JScience jar to make set() calls visible on matrices. Updated source code to reflect change.

--- Math/src/sriracha/math/wrappers/jscience/JsMatrix.java
+++ Math/src/sriracha/math/wrappers/jscience/JsMatrix.java
@@ -1,5 +1,8 @@
 package sriracha.math.wrappers.jscience;
 
+import org.jscience.mathematics.number.Float64;
+import org.jscience.mathematics.vector.ComplexMatrix;
+import org.jscience.mathematics.vector.Float64Matrix;
 import org.jscience.mathematics.vector.Matrix;
 import sriracha.math.interfaces.IMatrix;
 import sriracha.math.interfaces.IVector;
@@ -7,28 +10,17 @@ import sriracha.math.interfaces.IVector;
 abstract class JsMatrix implements IMatrix{
     
     protected Matrix matrix;
-    
-/*    protected int rows, columns;
-    
-    
-    private JsMatrix(Matrix m){
-        matrix = m;
-    }
-
-    public JsMatrix(int rows, int columns){
-        this.rows = rows;
-        this.columns = columns;
-    }
 
 
-    protected void buildMatrix(){}
-    
     @Override
     public IMatrix plus(IMatrix m) {
         if(m instanceof JsMatrix){
-            JsMatrix that = (JsMatrix)m;
-            buildMatrix();
-            return new JsMatrix(matrix.plus(that.matrix));
+
+            if(m instanceof JsComplexMatrix || this instanceof JsComplexMatrix){
+                return  new JsComplexMatrix(ComplexMatrix.valueOf(matrix.plus(((JsMatrix)m).matrix)));
+            }  else {
+                return new JsRealMatrix(Float64Matrix.valueOf(matrix.plus(((JsMatrix)m).matrix)));
+            }
         }
 
         return null;
@@ -37,9 +29,12 @@ abstract class JsMatrix implements IMatrix{
     @Override
     public IMatrix minus(IMatrix m) {
         if(m instanceof JsMatrix){
-            JsMatrix that = (JsMatrix)m;
-            buildMatrix();
-            return new JsMatrix(matrix.minus(that.matrix));
+            
+            if(m instanceof JsComplexMatrix || this instanceof JsComplexMatrix){
+                return  new JsComplexMatrix(ComplexMatrix.valueOf(matrix.minus(((JsMatrix)m).matrix)));
+            }  else {
+                return new JsRealMatrix(Float64Matrix.valueOf(matrix.minus(((JsMatrix)m).matrix)));
+            }
         }
 
         return null;
@@ -48,16 +43,33 @@ abstract class JsMatrix implements IMatrix{
     @Override
     public IMatrix times(IMatrix m) {
         if(m instanceof JsMatrix){
-            JsMatrix that = (JsMatrix)m;
-            buildMatrix();
-            return new JsMatrix(matrix.times(that.matrix));
+
+            if(m instanceof JsComplexMatrix || this instanceof JsComplexMatrix){
+                return  new JsComplexMatrix(ComplexMatrix.valueOf(matrix.times(((JsMatrix)m).matrix)));
+            }  else {
+                return new JsRealMatrix(Float64Matrix.valueOf(matrix.times(((JsMatrix)m).matrix)));
+            }
         }
 
         return null;
     }
 
     @Override
-    public IVector solve(IVector b) {
+    public IMatrix times(double n) {
+        if(this instanceof JsComplexMatrix){
+            return new JsComplexMatrix(ComplexMatrix.valueOf(matrix.times(Float64.valueOf(n))));
+        }  else {
+            return new JsRealMatrix(Float64Matrix.valueOf(matrix.times(Float64.valueOf(n))));
+        }
+    }
+    
+    @Override
+    public IVector solve(IVector b){
+        if(b instanceof JsVector) {
+            //todo: insert code here
+        }
 
-    }*/
+        return null;
+
+    }
 }
--- Math/src/sriracha/math/wrappers/jscience/JsRealMatrix.java
+++ Math/src/sriracha/math/wrappers/jscience/JsRealMatrix.java
@@ -1,21 +1,44 @@
 package sriracha.math.wrappers.jscience;
 
 import org.jscience.mathematics.number.Complex;
+import org.jscience.mathematics.number.Float64;
 import org.jscience.mathematics.vector.ComplexMatrix;
+import org.jscience.mathematics.vector.Float64Matrix;
 import sriracha.math.interfaces.IComplexMatrix;
 import sriracha.math.interfaces.IMatrix;
 import sriracha.math.interfaces.IRealMatrix;
 import sriracha.math.interfaces.IVector;
 
 public class JsRealMatrix extends JsMatrix implements IRealMatrix{
+
+
+
+
+   public JsRealMatrix(int m, int n){
+       matrix = Float64Matrix.valueOf(new double[m][n]);
+   }
+
+    JsRealMatrix(Float64Matrix m){
+        matrix = m;
+    }
+
+
+
+
+
+
+
+
+
+
     @Override
     public double getValue(int i, int j) {
-        return 0;
+        return ((Float64Matrix)matrix).get(i, j).doubleValue();
     }
 
     @Override
     public void setValue(int i, int j, double value) {
-
+        ((Float64Matrix)matrix).set(i, j, Float64.valueOf(value));
     }
 
     @Override
@@ -23,31 +46,4 @@ public class JsRealMatrix extends JsMatrix implements IRealMatrix{
         return new JsComplexMatrix(ComplexMatrix.valueOf( matrix.times(Complex.I)));
     }
 
-    @Override
-    public IMatrix plus(IMatrix m) {
-        return null;
-    }
-
-    @Override
-    public IMatrix minus(IMatrix m) {
-        return null;
-    }
-
-    public JsRealMatrix(int i, int j) {
-    }
-
-    @Override
-    public IMatrix times(IMatrix m) {
-        return null;
-    }
-
-    @Override
-    public IMatrix times(double n) {
-        return null;
-    }
-
-    @Override
-    public IVector solve(IVector b) {
-        return null;
-    }
 }
