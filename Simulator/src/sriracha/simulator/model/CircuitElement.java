71f33e36fce68309dc084ccede4bc5d806bb6b89
alennartz <alennartz@gmail.com>
2012-02-03 00:09:58 +0000

    partial checkin. - build broken - will fix soonish ...

--- Simulator/src/sriracha/simulator/model/CircuitElement.java
+++ Simulator/src/sriracha/simulator/model/CircuitElement.java
@@ -3,13 +3,40 @@ package sriracha.simulator.model;
 import sriracha.simulator.solver.interfaces.IEquation;
 
 /**
- * Base class for all circuit elements including sources
+ * Base class for all circuit elements including sources and subcircuits
  */
 public abstract class CircuitElement {
 
-    
+    /**
+     * Element name as described in the netlist
+     */
     public String name;
-    
+
+    /**
+     * Circuit Element Constructor
+     * @param name element name from netlist
+     */
+    protected CircuitElement(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Set the indices that correspond to the circuit element's nodes.
+     * The nodes are assumed to be in the order they are in the netlist.
+     * (-1 is always ground)
+     * @param indices the ordered node indices
+     */
+    public abstract void setNodeIndices(int ... indices);
+
+
+    /**
+     * Some elements add extra variables to the matrix. This method serves to set the index for the
+     * first such variables and should be overridden in all appropriate elements,
+     * the remaining variables take numbers sequentially.
+     * @param i index for the first of the extra variables required.
+     */
+    public void setFirstVarIndex(int i){}
+
     /**
      * Modifies the equation matrices by applying this elements stamp to them
      * @param equation - the equation to stamp.
@@ -27,5 +54,12 @@ public abstract class CircuitElement {
      * @return total number of variables this element represents in the final matrix representation
      */
     public abstract int getVariableCount();
+
+    /**
+     * This is used to build a copy of the circuit element during netlist parsing
+     * when adding multiple elements with the same properties.
+     * Node information will of course not be copied and have to be entered afterwards
+     */
+    public abstract CircuitElement buildCopy();
 
 }
