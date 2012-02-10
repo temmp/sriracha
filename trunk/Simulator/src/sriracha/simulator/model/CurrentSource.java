000d2e3ca391c953c548ad3c0a477d3a6d339380
alennartz@gmail.com <alennartz@gmail.com@8bf9ae03-dced-c7d4-3072-c6ccd60b3702>
2012-02-10 18:34:19 +0000

    fixed current source to be able to differentiate between dc or ac sources

--- trunk/Simulator/src/sriracha/simulator/model/CurrentSource.java
+++ trunk/Simulator/src/sriracha/simulator/model/CurrentSource.java
@@ -1,5 +1,6 @@
 package sriracha.simulator.model;
 
+import sriracha.math.MathActivator;
 import sriracha.math.interfaces.IComplex;
 import sriracha.simulator.solver.interfaces.IEquation;
 
@@ -9,16 +10,30 @@ public class CurrentSource extends Source {
      * complex number representation of phasor vector.
      */
     private IComplex currentPhasor;
+    private Double dcCurrent;
 
     /**
-     * Standard CurrentSource constructor
+     * Standard AC CurrentSource constructor
      *
-     * @param currentPhasor current in Amperes
+     * @param currentPhasor complex number representation of phasor vector
      * @param name name from netlist
      */
     public CurrentSource(String name, IComplex currentPhasor) {
         super(name);
         this.currentPhasor = currentPhasor;
+        dcCurrent = null;
+    }
+
+    /**
+     * Standard DC CurrentSource constructor
+     *
+     * @param dcCurrent current in Amperes
+     * @param name name from netlist
+     */
+    public CurrentSource(String name, double dcCurrent) {
+        super(name);
+        this.dcCurrent = dcCurrent;
+        currentPhasor = null;
     }
 
     /**
@@ -31,8 +46,14 @@ public class CurrentSource extends Source {
 
     @Override
     public void applyStamp(IEquation equation) {
-        equation.applySourceStamp(nMinus, currentPhasor);
-        equation.applySourceStamp(nPlus, currentPhasor.opposite());
+        if(dcCurrent == null){
+            equation.applySourceStamp(nMinus, currentPhasor);
+            equation.applySourceStamp(nPlus, currentPhasor.opposite());
+        }else {
+           IComplex val = MathActivator.Activator.complex(dcCurrent, 0);
+           equation.applySourceStamp(nMinus, val);
+           equation.applySourceStamp(nPlus, val.opposite());
+        }
     }
 
     @Override
