51fa616b7818606c1a690a911c136722b6e93620
alennartz@gmail.com <alennartz@gmail.com@8bf9ae03-dced-c7d4-3072-c6ccd60b3702>
2012-02-17 17:02:31 +0000

    finished data structures for .PLOT statement

--- trunk/Simulator/src/sriracha/simulator/solver/Voltage.java
+++ trunk/Simulator/src/sriracha/simulator/solver/Voltage.java
@@ -2,30 +2,32 @@ package sriracha.simulator.solver;
 
 import sriracha.math.interfaces.IComplex;
 import sriracha.math.interfaces.IComplexVector;
-import sriracha.simulator.solver.interfaces.IOutputData;
+import sriracha.simulator.solver.interfaces.OutputData;
 
 /**
  * Represents a voltage difference.
  * if nodeMinus is null ground is assumed.
  */
-public class Voltage implements IOutputData{
+public class Voltage extends OutputData {
     private Integer nodePlus, nodeMinus;
-
-
-    public Voltage(Integer nodePlus) {
+    
+    public Voltage(OutputType type, int nodePlus) {
+        super(type);
         this.nodePlus = nodePlus;
         nodeMinus = null;
     }
 
-    public Voltage(Integer nodePlus, Integer nodeMinus) {
+    public Voltage(OutputType type, Integer nodePlus, Integer nodeMinus) {
+        super(type);
         this.nodeMinus = nodeMinus;
         this.nodePlus = nodePlus;
     }
 
     @Override
-    public IComplex extract(IComplexVector data){
-        return nodeMinus != null ? data.getValue(nodePlus).minus(data.getValue(nodeMinus)):
-                                   data.getValue(nodePlus);
+    public double[] extract(IComplexVector data){
+        IComplex val = nodeMinus == null ? data.getValue(nodePlus) :
+                data.getValue(nodePlus).minus(data.getValue(nodeMinus));
+        return getFromType(val);
 
     }
 }
