6c7afac2adc4250713c1d47fcf91e26cfe721a91
darwish.michael@gmail.com <darwish.michael@gmail.com@8bf9ae03-dced-c7d4-3072-c6ccd60b3702>
2012-03-22 18:05:12 +0000

    Things can be deleted, basic element properties menu.

--- trunk/Frontend/gen/sriracha/frontend/R.java
+++ trunk/Frontend/gen/sriracha/frontend/R.java
@@ -33,38 +33,44 @@ containing a value of this type.
         public static final int circuitmenu_diodes_and_transistors=0x7f020003;
         public static final int circuitmenu_focusedborder=0x7f020004;
         public static final int circuitmenu_hand=0x7f020005;
-        public static final int circuitmenu_rlc=0x7f020006;
-        public static final int circuitmenu_select=0x7f020007;
-        public static final int circuitmenu_selectedborder=0x7f020008;
-        public static final int circuitmenu_sources_and_ground=0x7f020009;
-        public static final int circuitmenu_sub_circuits=0x7f02000a;
-        public static final int circuitmenu_wire=0x7f02000b;
-        public static final int sources_current=0x7f02000c;
-        public static final int sources_dependent_current=0x7f02000d;
-        public static final int sources_dependent_voltage=0x7f02000e;
-        public static final int sources_ground=0x7f02000f;
-        public static final int sources_vcc=0x7f020010;
-        public static final int sources_voltage=0x7f020011;
+        public static final int circuitmenu_itembackground=0x7f020006;
+        public static final int circuitmenu_rlc=0x7f020007;
+        public static final int circuitmenu_select=0x7f020008;
+        public static final int circuitmenu_selectedborder=0x7f020009;
+        public static final int circuitmenu_sources_and_ground=0x7f02000a;
+        public static final int circuitmenu_sub_circuits=0x7f02000b;
+        public static final int circuitmenu_wire=0x7f02000c;
+        public static final int sources_current=0x7f02000d;
+        public static final int sources_dependent_current=0x7f02000e;
+        public static final int sources_dependent_voltage=0x7f02000f;
+        public static final int sources_ground=0x7f020010;
+        public static final int sources_vcc=0x7f020011;
+        public static final int sources_voltage=0x7f020012;
     }
     public static final class id {
-        public static final int circuit_design_canvas=0x7f060007;
-        public static final int circuit_menu=0x7f060005;
-        public static final int circuit_menu_container=0x7f060004;
+        public static final int circuit_design_canvas=0x7f06000a;
+        public static final int circuit_menu=0x7f060007;
+        public static final int circuit_menu_container=0x7f060006;
         public static final int diodes_img=0x7f060002;
+        public static final int element_properties=0x7f060009;
+        public static final int hand=0x7f060005;
         public static final int rlc_img=0x7f060001;
-        public static final int sources_and_ground=0x7f060006;
-        public static final int sources_current=0x7f060009;
+        public static final int sources_and_ground=0x7f060008;
+        public static final int sources_current=0x7f06000c;
         public static final int sources_img=0x7f060000;
-        public static final int sources_voltage=0x7f060008;
+        public static final int sources_voltage=0x7f06000b;
         public static final int subcircuit_img=0x7f060003;
+        public static final int wire=0x7f060004;
     }
     public static final class layout {
         public static final int circuit_menu=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int sources_and_ground=0x7f030002;
+        public static final int element_properties=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int sources_and_ground=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int selectable=0x7f040001;
     }
     public static final class style {
         public static final int CircuitMenuItem=0x7f050000;
---
+++ trunk/Frontend/src/sriracha/frontend/android/CircuitDesignerMenu.java
@@ -0,0 +1,71 @@
+package sriracha.frontend.android;
+
+import android.view.*;
+import sriracha.frontend.*;
+
+import java.util.*;
+
+public class CircuitDesignerMenu
+{
+    private MainActivity context;
+
+    public CircuitDesignerMenu(MainActivity context)
+    {
+        this.context = context;
+    }
+
+    public void showSubMenu(int toShow)
+    {
+        for (View child : getAllSubMenus())
+        {
+            if (child.getId() == toShow)
+                child.setVisibility(View.VISIBLE);
+            else
+                child.setVisibility(View.GONE);
+        }
+    }
+
+    public ViewGroup getSelectedSubMenu()
+    {
+        for (View child : getAllSubMenus())
+        {
+            if (child.getVisibility() == View.VISIBLE)
+                return (ViewGroup) child;
+        }
+        return null;
+    }
+
+    public void setSelectedItem(int itemId)
+    {
+        ViewGroup selectedSubMenu = getSelectedSubMenu();
+        if (selectedSubMenu == null)
+            return;
+
+        for (View child : getAllSubViews(selectedSubMenu))
+        {
+            child.setSelected(child.getId() == itemId);
+        }
+    }
+
+    private View[] getAllSubMenus()
+    {
+        ViewGroup root = (ViewGroup) context.findViewById(R.id.circuit_menu_container);
+        View subMenus[] = new ViewGroup[root.getChildCount()];
+        for (int i = 0; i < root.getChildCount(); i++)
+            subMenus[i] = root.getChildAt(i);
+        return subMenus;
+    }
+
+    private ArrayList<View> getAllSubViews(ViewGroup root)
+    {
+        ArrayList<View> views = new ArrayList<View>();
+        for (int i = 0; i < root.getChildCount(); i++)
+        {
+            View child = root.getChildAt(i);
+            views.add(child);
+            if (child instanceof ViewGroup)
+                views.addAll(getAllSubViews((ViewGroup) child));
+        }
+        return views;
+    }
+}
