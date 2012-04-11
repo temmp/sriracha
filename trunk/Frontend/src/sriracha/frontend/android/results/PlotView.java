59779d38e341c49ffda7c2e97436ae2f484dd8d6
alennartz@gmail.com <alennartz@gmail.com@8bf9ae03-dced-c7d4-3072-c6ccd60b3702>
2012-04-11 23:37:27 +0000

    autoscaling of graph

--- trunk/Frontend/src/sriracha/frontend/android/results/PlotView.java
+++ trunk/Frontend/src/sriracha/frontend/android/results/PlotView.java
@@ -11,27 +11,43 @@ import sriracha.frontend.android.results.functions.Function;
 import sriracha.frontend.resultdata.Plot;
 import sriracha.frontend.resultdata.Point;
 
-public class PlotView extends View {
+public class PlotView extends View
+{
     private Plot plot;
 
     private Graph graph;
 
     private int color;
 
-    public void setFunc(Function func) {
+    public void setFunc(Function func)
+    {
         this.func = func;
     }
 
+
+    /**
+     * range of the data in this plot
+     *
+     * @return new double[]{xmin, xmax, ymin, ymax}
+     */
+    public double[] getRange()
+    {
+        return plot.getRange();
+    }
+
+
     private Function func;
 
-    public PlotView(Graph graph, Context context) {
+    public PlotView(Graph graph, Context context)
+    {
         super(context);
         this.graph = graph;
         init();
     }
 
     @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
         //  super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         int height = MeasureSpec.getSize(heightMeasureSpec);
         int width = MeasureSpec.getSize(widthMeasureSpec);
@@ -39,33 +55,39 @@ public class PlotView extends View {
         setMeasuredDimension(width, height);
     }
 
-    public PlotView(Context context, AttributeSet attrs) {
+    public PlotView(Context context, AttributeSet attrs)
+    {
         super(context, attrs);
         init();
     }
 
-    public PlotView(Context context, AttributeSet attrs, int defStyle) {
+    public PlotView(Context context, AttributeSet attrs, int defStyle)
+    {
         super(context, attrs, defStyle);
         init();
     }
 
-    private void init() {
+    private void init()
+    {
         //always stretch PlotView to size of graph
         setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         if (graph == null) graph = (Graph) getParent();
         color = Color.rgb(0, 250, 35);
     }
 
-    public void setColor(int color) {
+    public void setColor(int color)
+    {
         this.color = color;
     }
 
-    public void setPlot(Plot plot) {
+    public void setPlot(Plot plot)
+    {
         this.plot = plot;
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
+    protected void onDraw(Canvas canvas)
+    {
 
         Paint paint = new Paint();
         paint.setColor(color);
@@ -77,13 +99,15 @@ public class PlotView extends View {
 
         Point previous = index == 0 ? null : plot.getPoint(index - 1), p = null;
 
-        while (index < plot.size() && (index == 0 || plot.getPoint(index - 1).getX() <= graph.getXmax())) {
+        while (index < plot.size() && (index == 0 || plot.getPoint(index - 1).getX() <= graph.getXmax()))
+        {
             p = plot.getPoint(index);
 
             double pX = p.getX(), pY = func == null ? p.getY() : func.evaluate(p.getY());
 
 
-            if (previous == null || ((pY < graph.getYmin() || pY > graph.getYmax()) && (previous.getY() < graph.getYmin() || previous.getY() > graph.getYmax()))) {
+            if (previous == null || ((pY < graph.getYmin() || pY > graph.getYmax()) && (previous.getY() < graph.getYmin() || previous.getY() > graph.getYmax())))
+            {
                 previous = p;
                 index++;
                 continue;
@@ -97,7 +121,8 @@ public class PlotView extends View {
 
 
             //don't draw points that are really close together
-            if (Math.pow(end[0] - start[0], 2) + Math.pow(end[1] - start[1], 2) < 2) {
+            if (Math.pow(end[0] - start[0], 2) + Math.pow(end[1] - start[1], 2) < 2)
+            {
                 index++;
                 continue;
             }
@@ -118,7 +143,8 @@ public class PlotView extends View {
      * @param yCoord y coordinate of target point
      * @return closest point or null if none are currently visible
      */
-    public Point findNearestPoint(double xCoord, double yCoord) {
+    public Point findNearestPoint(double xCoord, double yCoord)
+    {
         int index = 0;
         Point touch = new Point(xCoord, yCoord);
         double minDistance = Double.POSITIVE_INFINITY;
@@ -128,9 +154,11 @@ public class PlotView extends View {
         while (index < plot.size() && plot.getPoint(index).getX() < graph.getXmin()) index++;
 
         //for points inside visible range
-        while (index < plot.size() && plot.getPoint(index).getX() <= graph.getXmax()) {
+        while (index < plot.size() && plot.getPoint(index).getX() <= graph.getXmax())
+        {
             Point p = plot.getPoint(index);
-            if (minDistance > touch.distance(p)) {
+            if (minDistance > touch.distance(p))
+            {
                 //use copy not actual point
                 closest = new Point(p.getX(), p.getY());
                 minDistance = touch.distance(closest);
